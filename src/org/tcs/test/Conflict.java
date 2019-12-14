ea429539f15e61b021bf3efa78b367d464d9a909
madhu8595 <madhu8595@gmail.com>
2019-12-14 14:19:08 +0530

    updated test2

--- src/org/tcs/test/Conflict.java
+++ src/org/tcs/test/Conflict.java
@@ -3,11 +3,8 @@ package org.tcs.test;
 public class Conflict {
 	public static void main(String[] args) {
 		System.out.println("master");
-<<<<<<< HEAD
-		System.out.println("developer");
-=======
 		System.out.println("tester");
->>>>>>> ca56eea09c2b4d6e2b8e5352da6f887d8bf62287
+		System.out.println("developer");
 	}
 
 }
